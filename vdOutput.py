3f3eb6df9524605bb20f031886c21972fdad8eb6
Anh <21T1hoanganh@gmail.com>
2023-10-02 22:03:33 +0700

    Create vdOutput.py

---
+++ vdOutput.py
@@ -0,0 +1,9 @@
+print('*'*10)
+print('{0:>2}/{1:>2}/{2:>4}'.format(2, 10, 2023))
+print('*'*10)
+
+print('-'*10)
+print('{0:>10}'.format("Quack"))
+print('{0:^10}'.format("Quack"))
+print('{0:<10}'.format("Quack"))
+print('-'*10)
