c5c6ba707ef96adaaa9785fda0c1b4d3971e8676
Anh <21T1hoanganh@gmail.com>
2023-10-02 22:04:08 +0700

    Create DiemTB.py

---
+++ DiemTB.py
@@ -0,0 +1,6 @@
+toan = float(input("Nhập điểm Toán: "))
+li = float(input("Nhập điểm Lí: "))
+hoa = float(input("Nhập điểm Hóa: "))
+dtb = (toan + li + hoa)/3
+print("Điểm trung bình:", dtb)
+print("Làm tròn:", round(dtb, 2))
