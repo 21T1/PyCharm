674c7df677df00b98e35217c5f801983238798bb
Anh <21T1hoanganh@gmail.com>
2023-10-02 22:03:36 +0700

    Create vdOperator.py

---
+++ vdOperator.py
@@ -0,0 +1,14 @@
+x = 5
+y = x**2
+print("x =", x, "& y =", y)
+
+print('*'*20)
+year = 2023
+t = year%2 == 0 and year%3 > 1
+print("t:", t)
+
+print("-"*20)
+print("5 is 5?", 5 is 5)
+
+print("_"*20)
+print("9 & 2:", 9/2, 9//2, 9%2)
