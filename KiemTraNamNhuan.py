eb7dfb573e2046506df2000e5d6036eeb033ceef
Anh <101346598+21T1@users.noreply.github.com>
2023-10-06 22:00:10 +0700

    Update KiemTraNamNhuan.py

--- KiemTraNamNhuan.py
+++ KiemTraNamNhuan.py
@@ -1,6 +1,6 @@
 print("Chương trình kiểm tra năm nhuận")
 year = int(input("Nhập vào 1 năm: "))
 if (year%4 == 0 and year%100 != 0) or year%400 == 0:
-    print("Năm",year,"là năm nhuận")
+    print("Năm", year, "là năm nhuận")
 else:
-    print("Năm",year,"không phải năm nhuận")
+    print("Năm", year, "không phải năm nhuận")
