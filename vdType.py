debf6510b37a10a98beab64ba4e1d58e0df28575
Anh <21T1hoanganh@gmail.com>
2023-10-09 21:45:30 +0700

    Create vdType.py

---
+++ vdType.py
@@ -0,0 +1,19 @@
+x = 5
+print(type(x))
+
+x = 5.5
+print(type(x))
+
+x = 'Happy new year!'
+print(type(x))
+
+x = True
+print(type(x))
+
+x = complex(3, 2)
+print(type(x))
+print(x.real, "+", x.imag, "i")
+
+
+
+
