4c5ff7b18f1e865e01a2e3bb87a885c06f035b78
Anh <21T1hoanganh@gmail.com>
2023-10-10 21:42:38 +0700

    Update DiemTB.py

--- DiemTB.py
+++ DiemTB.py
@@ -4,3 +4,11 @@ hoa = float(input("Nhập điểm Hóa: "))
 dtb = (toan + li + hoa)/3
 print("Điểm trung bình:", dtb)
 print("Làm tròn:", round(dtb, 2))
+
+print("Chương trình tính điểm trung bình")
+toan, li, hoa = eval(input("Nhập điểm toán, lý, hóa: "))
+print("Điểm toán:", toan)
+print("Điểm lí:", li)
+print("Điểm hóa:", hoa)
+dtb = (toan + li + hoa)/3
+print("Điểm trung bình:", round(dtb,2))
